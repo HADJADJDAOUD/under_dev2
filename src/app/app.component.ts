11c9e1657c8b2cc9f1defe9e4602bd2d637cdd8e
HADJADJDAOUD <daoudhdj@gmail.com>
2024-12-08 23:40:38 +0100

    getting-used-to-it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,30 @@
 import { Component } from '@angular/core';
 
 import { HomeComponent } from './home/home.component';
-
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ HomeComponent,],
-  
+  imports: [ RouterModule],
+
   template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-`,
-  styleUrl: './app.component.css'
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img
+            class="brand-logo"
+            src="/assets/logo.svg"
+            alt="logo"
+            aria-hidden="true"
+          />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Daoud';
